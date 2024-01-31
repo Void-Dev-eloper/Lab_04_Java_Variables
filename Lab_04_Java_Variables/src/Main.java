b94122f61bc34a571bbfa932e6556461c4a19b02
ZN-Zz <badhikari1211@gmail.com>
2024-01-31 10:27:50 -0500

    Java Updated

---
+++ Lab_04_Java_Variables/src/Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+
+
+//        PART 2:
+
+        int kidsInFamily = 3;
+        String isItRaining = "No";
+        double priceOfGas = 3.52;
+        int favoriteNumber = 15;
+        double shoeSize = 10.5;
+        int birthMonth = 2;
+        String fullName = "Bibash Adhikari";
+
+        System.out.println("The number of kids in my Family is :" + kidsInFamily);
+        System.out.println("Is it raining? " + isItRaining);
+        System.out.println("Price per gallon of Gas: $" + priceOfGas);
+        System.out.println("My favorite number is: " + favoriteNumber);
+        System.out.println("My shoe size is: " + shoeSize);
+        System.out.println("My birth month is: " + birthMonth);
+        System.out.println("My full name is: " + fullName);
+
+
+
+    }
+}
